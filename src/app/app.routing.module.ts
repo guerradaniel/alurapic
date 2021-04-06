7838c0b89d73e8867481a55ecd0f940b3d55a6ea
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-04-06 00:20:54 -0300

    Inserir Resolver para melhorar o carregamento da página.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,22 +3,28 @@ import { Routes, RouterModule } from '@angular/router'
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list-resolver';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 const routes: Routes = [
-    { path: 'user/:userName', component: PhotoListComponent },
-    { path: 'p/add', component: PhotoFormComponent },
-    { path: '**', component: NotFoundComponent }
+	{
+		path: 'user/:userName', component: PhotoListComponent,
+		resolve: {
+			photos: PhotoListResolver
+		}
+	},
+	{ path: 'p/add', component: PhotoFormComponent },
+	{ path: '**', component: NotFoundComponent }
 
 ]
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes)
-    ],
-    exports: [
-        RouterModule
-    ]
+	imports: [
+		RouterModule.forRoot(routes)
+	],
+	exports: [
+		RouterModule
+	]
 })
 export class AppRoutingModule {
 
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -14,17 +14,11 @@ export class PhotoListComponent implements OnInit {
   filter: string = ''
 
   constructor(
-    private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-
-    const userName = this.activatedRoute.snapshot.params.userName
-    this.photoService.listFromUser(userName)
-      .subscribe(photos =>
-        this.photos = photos
-      )
+    this.photos = this.activatedRoute.snapshot.data['photos']
   }
 
 }
