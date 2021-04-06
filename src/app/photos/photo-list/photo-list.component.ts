03d64b8037b3a20b4095404c1f7b8f3c455c6899
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-04-06 00:44:06 -0300

    Usei Subject do RxJS para melhorar performance.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
 
 import { Photo } from '../photo/photo';
 
@@ -12,6 +14,7 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = []
   filter: string = ''
+  debounce: Subject<string> = new Subject<string>()
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -19,6 +22,9 @@ export class PhotoListComponent implements OnInit {
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos']
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter)
   }
 
 }
