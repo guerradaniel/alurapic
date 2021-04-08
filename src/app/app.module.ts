8e0aae99f1082b8885caa78970cbfeb6488e1fbe
Daniel Guerra <danielguerra.g12@gmail.com>
2021-04-08 00:38:44 -0300

    Inseri um tela de login na home.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HomeModule,
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
-    ErrorsModule
+    ErrorsModule,
   ],
   providers: [
   ],
