64a5e8de35aceba8bdc63430ae8c2e94c48a4977
Daniel Guerra <danielguerra.g12@gmail.com>
2021-04-12 23:40:44 -0300

    Adicionei User service para guardar token com BehaviorSubject.

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { TokenService } from '../token/token.service';
+
+import * as jtw_decode from 'jwt-decode'
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private userSubject = new BehaviorSubject<User>(null)
+
+  constructor(
+    private tokenService: TokenService
+  ) {
+    this.tokenService.hasToken() &&
+      this.decodeAndNotify()
+  }
+
+  setToken(token: string) {
+    this.tokenService.setToken(token)
+    this.decodeAndNotify()
+  }
+
+  getUser() {
+    return this.userSubject.asObservable();
+  }
+
+  private decodeAndNotify() {
+    const token = this.tokenService.getToken() // setei o token
+    const user = jtw_decode(token) as User; // peguei token salvo, pego o payload
+    this.userSubject.next(user) // transformo para tipo User
+  }
+}
