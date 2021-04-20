d9c79829c7797e92dc5c7a9153e9095ec1821118
Daniel Guerra <danielguerra.g12@gmail.com>
2021-04-20 14:39:31 -0300

    Adicionei component e formulário de registro.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router'
 import { AuthGuard } from './core/auth/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { SignInComponent } from './home/signin/signin.component';
+import { SignUpComponent } from './home/signup/signup.component';
 
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list-resolver';
@@ -10,6 +11,7 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 const routes: Routes = [
 	{ path: '', component: SignInComponent, canActivate: [AuthGuard] },
+	{ path: 'signup', component: SignUpComponent },
 	{
 		path: 'user/:userName', component: PhotoListComponent,
 		resolve: {
