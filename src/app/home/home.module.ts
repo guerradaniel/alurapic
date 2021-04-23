3fd3464026bd72054e86d3de9bcf111078e477dd
Daniel Guerra <danielguerra.g12@gmail.com>
2021-04-23 20:23:54 -0300

    Criei o envio de token através de um interceptor.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.rounting.module';
+import { SignUpService } from './signup/signup.service';
 
 @NgModule({
     declarations: [
@@ -21,6 +22,9 @@ import { HomeRoutingModule } from './home.rounting.module';
         VMessageModule,
         RouterModule,
         HomeRoutingModule
+    ],
+    providers: [
+        SignUpService
     ]
 })
 export class HomeModule {
